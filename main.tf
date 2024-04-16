b0779e8fd55e3c2546edcb4d397583067d81f9a9
Saksham Awasthi <74664860+Cipher-08@users.noreply.github.com>
2024-04-16 11:51:43 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,11 +1,19 @@
 provider "aws" {
-  region = "global"
-  access_key = ""
-  secret_key = ""
+  region = "us-east-1"
+  access_key = var.aws_access_key
+  secret_key = var.aws_secret_key
 }
 
 resource "aws_s3_bucket" "aws_s3_env0_bucket_test" {
   bucket = "aws_s3_env0_bucket_test_4398438"
 }
 
+variable "aws_access_key" {
+  type = string
+  default = ""
+}
 
+variable "aws_secret_key" {
+  type = string
+  default = ""
+}
