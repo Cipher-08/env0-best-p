3075a7d13c80003d6c5746b1df1903435e713dc2
Saksham Awasthi <74664860+Cipher-08@users.noreply.github.com>
2024-04-16 12:24:15 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -17,3 +17,8 @@ variable "aws_secret_key" {
   type = string
   default = ""
 }
+
+module "modules_reg" {
+  source = "api.env0.com/81b8f9f3-6542-417b-a2b8-e8120df3a2a2/modules_reg/org"
+  version = "1.0.0"
+}
