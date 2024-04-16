a6b01190db19cdbad187bc0af9d80eb0758df2ad
Saksham Awasthi <74664860+Cipher-08@users.noreply.github.com>
2024-04-16 15:24:07 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,15 +1,12 @@
 provider "aws" {
-  region = var.aws_region
+  region = "us-east-1"
   access_key = var.aws_access_key
   secret_key = var.aws_secret_key
 }
 
 resource "aws_s3_bucket" "aws-s3-env0-bucket-test-1" {
   bucket = "aws-env0-bucket-test-4398438-1"
-tags = {
-    Name        = "Nameofthebucket"
-    Environment = "Production"
-  }
+  tags = var.bucket_tags
 }
 
 variable "aws_access_key" {
@@ -17,15 +14,19 @@ variable "aws_access_key" {
   default = ""
 }
 
-variable "aws_region" {
-  type = string
-  default = ""
+variable "bucket_tags" {
+  type        = map(string)
+  default = {
+  
+  }
 }
 
 module "module" {
   source = "api.env0.com/81b8f9f3-6542-417b-a2b8-e8120df3a2a2/module/org"
   version = "1.1.6"
 }
+
+
 
 variable "aws_secret_key" {
   type = string
