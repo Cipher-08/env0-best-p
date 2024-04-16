617c21e807a8c395896759200d8a58c727ac3eea
Saksham Awasthi <74664860+Cipher-08@users.noreply.github.com>
2024-04-16 14:55:32 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 provider "aws" {
-  region = "us-east-1"
+  region = var.aws_region
   access_key = var.aws_access_key
   secret_key = var.aws_secret_key
 }
@@ -15,6 +15,11 @@ tags = {
 variable "aws_access_key" {
   type = string
   default = ""
+}
+
+module "module" {
+  source = "api.env0.com/81b8f9f3-6542-417b-a2b8-e8120df3a2a2/module/org"
+  version = "1.1.2"
 }
 
 variable "aws_secret_key" {
