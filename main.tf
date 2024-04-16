e8fb96baf33c65956be001bc2ca1c84b0eac7293
Saksham Awasthi <74664860+Cipher-08@users.noreply.github.com>
2024-04-16 10:44:38 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,8 +1,20 @@
 provider "aws" {
   region = "us-east-1"
+  access_key = var.access
+  secret_access_key = var.secret
 }
 
 resource "aws_s3_bucket" "my_s3_bucket" {
   bucket = "my-s3-bucket-${timestamp()}"
   acl    = "private"
+}
+
+variable "access" {
+  type = string
+  default = ""
+}
+
+variable "secret" {
+  type = string
+  default = ""
 }
