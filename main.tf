8aaae059b1b9b1ae01f6479eb9e1942e5eaed467
Saksham Awasthi <74664860+Cipher-08@users.noreply.github.com>
2024-04-17 13:27:08 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,33 +1,36 @@
 provider "aws" {
-  region = "us-east-1"
+  region     = "us-east-1"
   access_key = var.aws_access_key
   secret_key = var.aws_secret_key
 }
 
-resource "aws_s3_bucket" "aws-s3-env0-bucket-test-113" {
-  bucket = "aws-env0-bucket-test-4398438-113"
- # tags = var.bucket_tags
+variable "aws_access_key" {
+  type    = string
+  default = ""
 }
 
-variable "aws_access_key" {
-  type = string
+variable "aws_secret_key" {
+  type    = string
   default = ""
+}
+
+variable "bucket_name" {
+  type    = string
+  default = "aws-env0-bucket-test-4398438-113"
+}
+
+resource "aws_s3_bucket" "aws-s3-env0-bucket-test-113" {
+  bucket = var.bucket_name
+  # Uncomment and update the following line if you have bucket tags defined
+  # tags = var.bucket_tags
 }
 
 # variable "bucket_tags" {
-#   type        = map(string)
-#   default = {
-  
-#   }
+#   type    = map(string)
+#   default = {}
 # }
 
 # module "module" {
-#   source = "api.env0.com/81b8f9f3-6542-417b-a2b8-e8120df3a2a2/module/org"
+#   source  = "api.env0.com/81b8f9f3-6542-417b-a2b8-e8120df3a2a2/module/org"
 #   version = "1.1.6"
 # }
-
-variable "aws_secret_key" {
-  type = string
-  default = ""
-}
-
