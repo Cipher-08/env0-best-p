665b343ee435ad1197fe9b63e709c5c97aba4975
Saksham Awasthi <74664860+Cipher-08@users.noreply.github.com>
2024-04-18 12:40:17 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,7 +5,7 @@ provider "aws" {
 }
 
 resource "aws_s3_bucket" "aws-s3-env0-bucket-test-11" {
-  bucket = "aws-env0-bucket-test-4398438-11"
+  bucket = "aws-env0-bucket-test-4398438-11224342"
  # tags = var.bucket_tags
 }
 
