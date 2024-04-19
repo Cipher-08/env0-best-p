f028fa4a6327bf9e1df1992a6d62716fe8fe57f1
Saksham Awasthi <74664860+Cipher-08@users.noreply.github.com>
2024-04-19 16:27:38 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,18 +1,11 @@
-terraform {
-  backend "s3" {
-    bucket = "aws-env0-bucket-test-4398438"
-    region = "us-east-1"
-  }
-}
-
 provider "aws" {
   region = "us-east-1"
   access_key = var.aws_access_key
   secret_key = var.aws_secret_key
 }
 
-resource "aws_s3_bucket" "aws-s3-env0-bucket-test-11" {
-  bucket = "aws-env0-bucket-test-4398438-754574"
+resource "aws_s3_bucket" "aws-s3-env0-bucket-test-11843" {
+  bucket = "aws-env0-bucket-test-4398438-7545798434"
  # tags = var.bucket_tags
 }
 
